66793014a9e9015aaae14aec259ce8a942c86022
Burak Köse <burakkose011@icloud.com>
2024-05-03 14:14:08 +0300

    program tamamlandi

--- hipotenusHesaplama/src/Main.java
+++ hipotenusHesaplama/src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        /*
+        Dik Üçgende Hipotenüs Bulan Program
+        Java ile kullanıcıdan dik kenarlarının uzunluğunu alan ve hipotenüsü hesaplayan programı yazın.
+
+        Ödev
+            Üç kenar uzunluğunu kullanıcıdan aldığınız üçgenin alanını hesaplayan programı yazınız.
+
+        Formül
+            Üç𝑔𝑒𝑛𝑖𝑛 ç𝑒𝑣𝑟𝑒𝑠𝑖 = 2𝑢
+
+            𝑢 = (a+b+c) / 2
+
+            Alan * Alan = 𝑢 * (𝑢 − 𝑎)* (𝑢 − 𝑏) * (𝑢 − 𝑐)
+         */
+
+        int a,b;
+        double c;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("A kenarı Girin : ");
+        a = input.nextInt();
+
+        System.out.print("B kenarı Girin : ");
+        b = input.nextInt();
+
+
+        // karekök alma
+
+        c = Math.sqrt((a*a) + (b*b));
+
+        System.out.println("Hipotenüs : " + c);
+
+
 
     }
 }
