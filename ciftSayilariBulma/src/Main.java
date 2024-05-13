8c9bd3ea969cc7f3337d7a5363f5c19a2e470bf3
Burak Köse <burakkose011@icloud.com>
2024-05-14 01:22:52 +0300

    program tamamlandi

--- ciftSayilariBulma/src/Main.java
+++ ciftSayilariBulma/src/Main.java
@@ -1,5 +1,28 @@
-public class Main{
-    public static void main(String[] args){
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        Java döngüler ile kullanıcının girdiği sayıya kadar çift olan sayıları bulan programı yazıyoruz.
+
+        Ödev
+        Java döngüler ile 0'dan girilen sayıya kadar olan sayılardan 3 ve 4'e tam bölünen sayıların ortalamasını hesaplayan programı yazınız.
+         */
+        int k,i = 1;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Bir sayı girin : ");
+        k = inp.nextInt();
+
+        while(k >= i){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+            i++;
+        }
+
 
     }
 }
