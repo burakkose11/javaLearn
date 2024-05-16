9241e3b022659d3fa2493a52b2658b5162eb0b20
Burak Köse <burakkose011@icloud.com>
2024-05-16 14:45:58 +0300

    proje tamamlandi

--- usluSayiHesaplama/src/Main.java
+++ usluSayiHesaplama/src/Main.java
@@ -1,5 +1,24 @@
-public class Main{
-    public static void main(String[] args){
-        
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int n, u, total = 1;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Bir sayı giriniz : ");
+        n = inp.nextInt();
+
+        System.out.print("Üs sayısını giriniz : ");
+        u = inp.nextInt();
+
+        for (int i = 1; i <= u; i++) {
+            total *= n;
+        }
+
+        System.out.println(n + " in " + u + " üssü = " + total);
+
+
     }
 }
