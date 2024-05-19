5f64afec878e8acfc6e8e02f4e8be75d1848465a
Burak Köse <burakkose011@icloud.com>
2024-05-19 15:07:29 +0300

    proje tamamlandi

--- yildizlarlaUcgenYapimi/src/Main.java
+++ yildizlarlaUcgenYapimi/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+import java.util.jar.JarOutputStream;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        int yildiz;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Yıldız sayısını girin : ");
+        yildiz = inp.nextInt();
+
+
+        for (int i = 1; i <= yildiz; i++) {
+
+            for (int k = 1; k <= yildiz - i; k++) {
+                System.out.print(" ");
+            }
+
+            for (int y = 1; y <= (i * 2) - 1; y++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+
     }
 }
