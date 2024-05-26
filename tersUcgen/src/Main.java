9e4413ab6fd3f9e65c1063bfc43a38ff137a7176
Burak Köse <burakkose011@icloud.com>
2024-05-26 17:49:54 +0300

    proje tamamlandi

--- tersUcgen/src/Main.java
+++ tersUcgen/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Yıldız sayısınız girin : ");
+        int n = inp.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; k < (2 * (n - i)) - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
     }
 }
