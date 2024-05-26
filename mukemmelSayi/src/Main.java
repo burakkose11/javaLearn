a0f9d6ba1bcb4b8a41b75e9a8607ee175945010f
Burak Köse <burakkose011@icloud.com>
2024-05-26 17:31:12 +0300

    proje tamamlandi

--- mukemmelSayi/src/Main.java
+++ mukemmelSayi/src/Main.java
@@ -1,4 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+
+        int number, total = 0, select;
+
+
+        System.out.print("Bir sayı girin : ");
+        number = inp.nextInt();
+
+
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                total += i;
+            }
+
+        }
+
+        if (total == number) {
+            System.out.println(number + " Mükemmel bir sayıdır.");
+        } else {
+            System.out.println(number + " Mükemmel bir sayı değildir.");
+        }
+
+
     }
 }
