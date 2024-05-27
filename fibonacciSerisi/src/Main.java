503d6f222d9b20620b5fd8e0f98aeedd4eeecb70
Burak Köse <burakkose011@icloud.com>
2024-05-27 09:00:59 +0300

    proje tamamlandi

--- fibonacciSerisi/src/Main.java
+++ fibonacciSerisi/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Fibonacci sayısını girin : ");
+        int n = sc.nextInt();
+
+        int s1 = 0;
+        int s2 = 1;
+        int toplam = 0;
+
+        // fibonacci : 0,1,1,2,3,5,8
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(s1 + " , ");
+
+            toplam = s1 + s2;
+            s1 = s2;
+            s2 = toplam;
+
+        }
+
+
     }
 }
