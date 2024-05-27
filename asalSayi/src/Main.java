186d86e7f56617902954d4b9e2fa2ea0ca30e135
Burak Köse <burakkose011@icloud.com>
2024-05-27 08:36:26 +0300

    proje tamamlandi

--- asalSayi/src/Main.java
+++ asalSayi/src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        int n = 100;
+
+        for (int i = 2; i < n; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(i);
+                System.out.print(" ");
+            }
+
+        }
+
+
     }
 }
