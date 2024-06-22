ed783ce7447f62f676d0f0bd3ae832d3c4592a7e
Burak Köse <burakkose011@icloud.com>
2024-06-22 17:48:07 +0300

    proje tamamlandi

--- recursiveUsluSayi/src/Main.java
+++ recursiveUsluSayi/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
-    public static void main(String[] args) {
 
+    static int power(int base, int exponent) {
+        int result = 1;
+
+        for (int i = 1; i <= exponent; i++) {
+            result *= base;
+        }
+
+        System.out.println(result);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        power(2, 3);
     }
 }
