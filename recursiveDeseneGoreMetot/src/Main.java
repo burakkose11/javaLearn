4a8326503f41ece181231de7d1c7ebf6bed1881e
Burak Köse <burakkose011@icloud.com>
2024-06-24 00:09:39 +0300

    proje tamamlandi

--- recursiveDeseneGoreMetot/src/Main.java
+++ recursiveDeseneGoreMetot/src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static int minusNumber(int processValue) {
+
+        if (processValue <= 0) return processValue;
+        System.out.print(processValue + " ");
+
+
+        return minusNumber(processValue - 5);
+    }
+
+    static void plusNumber(int processValue, int userInput) {
+
+        if (userInput >= processValue) {
+            System.out.print(processValue + " ");
+            plusNumber(processValue + 5, userInput);
+        }
+
+
+    }
+
+
     public static void main(String[] args) {
-        
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Bir sayı girin : ");
+        int userInput = inp.nextInt();
+
+        int processValue = minusNumber(userInput);
+
+        plusNumber(processValue, userInput);
+
+
     }
 }
